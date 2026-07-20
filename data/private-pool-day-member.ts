d1d1e8d2fc2ca5e9a36084b22fe68edd24114943
chamideviler-arch <chamideviler@gmail.com>
2026-07-20 23:37:44 +0300

    Add private pool day member image data

---
+++ data/private-pool-day-member.ts
@@ -0,0 +1,60 @@
+export type PrivatePoolMemberImage = {
+  src: string;
+  alt: string;
+  downloadHref?: string;
+};
+
+export const privatePoolDayMemberImages: PrivatePoolMemberImage[] = [
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-01.webp",
+    alt: "Sofia private pool day swimwear portrait 01",
+    downloadHref: "/images/collections/private-pool-day/private-pool-day-01.webp",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-02.webp",
+    alt: "Sofia private pool day casual selfie 02",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-03.webp",
+    alt: "Sofia private pool day summer lifestyle portrait 03",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-04.webp",
+    alt: "Sofia private pool day poolside portrait 04",
+    downloadHref: "/images/collections/private-pool-day/private-pool-day-04.webp",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-05.webp",
+    alt: "Sofia private pool day relaxed summer moment 05",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-06.webp",
+    alt: "Sofia private pool day swimwear portrait 06",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-07.webp",
+    alt: "Sofia private pool day casual poolside selfie 07",
+    downloadHref: "/images/collections/private-pool-day/private-pool-day-07.webp",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-08.webp",
+    alt: "Sofia private pool day summer lifestyle portrait 08",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-09.webp",
+    alt: "Sofia private pool day relaxed poolside photo 09",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-10.webp",
+    alt: "Sofia private pool day swimwear portrait 10",
+    downloadHref: "/images/collections/private-pool-day/private-pool-day-10.webp",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-11.webp",
+    alt: "Sofia private pool day casual selfie 11",
+  },
+  {
+    src: "/images/collections/private-pool-day/private-pool-day-12.webp",
+    alt: "Sofia private pool day summer moment 12",
+  },
+];
